32b305023a94103f296f8b0d9d7fd86f7f602c07
Philippe Duveau <philippe.duveau@pmu.fr>
2019-02-12 11:17:00 +0100

    Adding license header to source code

--- src/proclinux.h
+++ src/proclinux.h
@@ -1,3 +1,36 @@
+/* proclinux.h
+ *
+ * Implementation of the linux performance collector.
+ *
+ * As the module was first written on AIX plate-form using getproc64 interface,
+ * this file is an getproc64 like for linux. This allow to keep a very closed
+ * core program code available for those two OS.
+ *
+ * File begun on 2015-09-06 by Philippe Duveau as a rsyslog input module.
+ *
+ * Now it has been extracted to an external tool which can be integrated as
+ * an event provider of rsyslog improg (new input module)
+ *
+ * Copyright 2019 Philippe Duveau and Pari Mutuel Urbain.
+ *
+ * This file is part of jsonperfmon.
+ *
+ * Jsonperfmon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jsonperfmon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jsonperfmon.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * A copy of the GPL can be found in the file "COPYING" in this distribution.
+ */
+
 
 #if !defined(_H_PROCLINUX) && !defined(_AIX)
 #define	_H_PROCLINUX
